4ab680ad0eea8cfcc0de8c8946f1e624d10fc69e
GustavoFreitas22 <gusfmotta@gmail.com>
2021-07-29 10:36:49 -0300

    Criacao da model Tema

---
+++ APonte/src/main/java/com/aponte/APonte/model/Tema.java
@@ -0,0 +1,75 @@
+package com.aponte.APonte.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String saude;
+	
+	@NotNull
+	private String alimentacao;
+	
+	@NotNull
+	private String educacao;
+	
+	@NotNull
+	private String descricao;
+	
+	public Tema() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getSaude() {
+		return saude;
+	}
+
+	public void setSaude(String saude) {
+		this.saude = saude;
+	}
+
+	public String getAlimentacao() {
+		return alimentacao;
+	}
+
+	public void setAlimentacao(String alimentacao) {
+		this.alimentacao = alimentacao;
+	}
+
+	public String getEducacao() {
+		return educacao;
+	}
+
+	public void setEducacao(String educacao) {
+		this.educacao = educacao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
