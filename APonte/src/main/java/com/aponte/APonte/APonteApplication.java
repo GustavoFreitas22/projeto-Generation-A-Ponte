138c42a8f52a67b29cf78eb0ba46ddbe814ae661
GustavoFreitas22 <gusfmotta@gmail.com>
2021-07-29 09:50:34 -0300

    Criacao projeto Spring

---
+++ APonte/src/main/java/com/aponte/APonte/APonteApplication.java
@@ -0,0 +1,13 @@
+package com.aponte.APonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APonteApplication.class, args);
+	}
+
+}
---
+++ APonte/src/test/java/com/aponte/APonte/APonteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aponte.APonte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APonteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
